b99c03ccf5c16783deda59e6cad1bfab665b3fb8
aissatou0 <kanoutea39@gmail.com>
2025-12-29 19:47:07 +0000

    page gestion des complements avec ajout, modif et archivage/desarchivage de complement

--- symfony/brasil-burger-symfony/src/Controller/Admin/ComplementController.php
+++ symfony/brasil-burger-symfony/src/Controller/Admin/ComplementController.php
@@ -3,17 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Complement;
-use App\Repository\BurgerRepository;
 use App\Repository\ComplementRepository;
-use Cloudinary\Cloudinary;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Cloudinary\Cloudinary;
 
-// src/Controller/Admin/ComplementController.php
 #[Route('/admin/complements', name: 'admin_complements_')]
 class ComplementController extends AbstractController
 {
@@ -24,4 +21,100 @@ class ComplementController extends AbstractController
             'complements' => $repository->findAll()
         ]);
     }
+
+    #[Route('/add', name: 'add', methods: ['POST'])]
+    public function add(
+        Request $request,
+        EntityManagerInterface $em
+    ): Response {
+        $name  = trim($request->request->get('name'));
+        $prix  = $request->request->get('prix');
+        $type  = $request->request->get('typeComplement');
+        $file  = $request->files->get('image');
+
+        if (!$name || !$prix || !$type || !$file) {
+            $this->addFlash('danger', 'Tous les champs sont obligatoires');
+            return $this->redirectToRoute('admin_complements_index');
+        }
+
+        // ☁️ Cloudinary
+        $cloudinary = new Cloudinary($_ENV['CLOUDINARY_URL']);
+
+        $upload = $cloudinary->uploadApi()->upload(
+            $file->getRealPath(),
+            [
+                'folder' => 'brasil-burger/complements',
+                'public_id' => uniqid('complement_'),
+                'resource_type' => 'image'
+            ]
+        );
+
+        $complement = new Complement();
+        $complement->setName($name);
+        $complement->setPrix($prix);
+        $complement->setTypeComplement($type);
+        $complement->setImage($upload['secure_url']);
+        $complement->setActif(true);
+
+        $em->persist($complement);
+        $em->flush();
+
+        $this->addFlash('success', 'Complément ajouté avec succès');
+
+        return $this->redirectToRoute('admin_complements_index');
+    }
+    #[Route('/{id}/toggle', name: 'toggle', methods: ['POST'])]
+public function toggle(
+    Complement $complement,
+    EntityManagerInterface $em
+): Response {
+    $complement->setActif(!$complement->isActif());
+    $em->flush();
+
+    return $this->redirectToRoute('admin_complements_index');
+}
+
+#[Route('/{id}/edit', name: 'edit', methods: ['POST'])]
+public function edit(
+    Complement $complement,
+    Request $request,
+    EntityManagerInterface $em
+): Response {
+    $name  = trim($request->request->get('name'));
+    $prix  = $request->request->get('prix');
+    $type  = $request->request->get('typeComplement');
+    $file  = $request->files->get('image');
+
+    if (!$name || !$prix || !$type) {
+        $this->addFlash('danger', 'Champs obligatoires manquants');
+        return $this->redirectToRoute('admin_complements_index');
+    }
+
+    $complement->setName($name);
+    $complement->setPrix($prix);
+    $complement->setTypeComplement($type);
+
+    // 🔄 Image facultative
+    if ($file) {
+        $cloudinary = new Cloudinary($_ENV['CLOUDINARY_URL']);
+
+        $upload = $cloudinary->uploadApi()->upload(
+            $file->getRealPath(),
+            [
+                'folder' => 'brasil-burger/complements',
+                'public_id' => uniqid('complement_'),
+                'resource_type' => 'image'
+            ]
+        );
+
+        $complement->setImage($upload['secure_url']);
+    }
+
+    $em->flush();
+
+    $this->addFlash('success', 'Complément modifié avec succès');
+
+    return $this->redirectToRoute('admin_complements_index');
+}
+
 }
